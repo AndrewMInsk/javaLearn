a5b24544c5fe8d8f80d3633a99992f43886b92e0
A-SITE <info@a-site.by>
2020-02-17 23:53:22 +0300

    final28875656

---
+++ src/group1/task1_1/Solution.java
@@ -0,0 +1,16 @@
+package group1.task1_1;
+
+public class Solution {
+    public static void main(String[] args) {
+        double a = 4.233, b = 3;
+        double division = a/b;
+        double product = a*b;
+        double sum = a+b;
+        double del = a-b;
+        System.out.println(division);
+        System.out.println(product);
+        System.out.println(sum);
+        System.out.println(del);
+    }
+
+}
---
+++ src/group2/task2_2/ghjj.java
@@ -0,0 +1,4 @@
+package group2.task2_2;
+
+public class ghjj {
+}
